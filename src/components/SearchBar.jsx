2dbc868d599b648df09197222799e6d619191c06
Faizan <faizan2017.fk@gmail.com>
2023-10-15 11:49:23 +0530

    SearchBar Commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import React, { useState } from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
+import { words } from '../lib/data'
+
+const SearchBar = () => {
+
+    const [activeSearch, setActiveSearch] = useState([])
+
+    const handleSearch = (e) => {
+        if(e.target.value == ''){
+            setActiveSearch([])
+            return false
+        }
+        setActiveSearch(words.filter(w => w.includes(e.target.value)).slice(0,8))
+    }
+
+  return (
+    <form className='relative'>
+        <div className="relative">
+            <input type="search" placeholder='Type Here' className='w-full p-4 rounded-full bg-slate-800 lg:w-80' onChange={(e) => handleSearch(e)}/>
+            <button className='absolute right-1 top-1/2 -translate-y-1/2 p-4 bg-slate-600 rounded-full'>
+                <AiOutlineSearch />
+            </button>
+        </div>
+
+        {
+            activeSearch.length > 0 && (
+                <div className="absolute top-20 p-4 bg-slate-800 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2">
+                    {
+                        activeSearch.map(s => (
+                            <span>{s}</span>
+                        ))
+                    }
+                </div>
+            )
+        }
+
+
+        
+    </form>
+  )
+}
+
+export default SearchBar
---
+++ src/lib/data.js
@@ -0,0 +1,13 @@
+export const words= [
+    'ac',
+    'AC',
+    'A.C.',
+    'A.c',
+    'a.C',
+    'a.c.',
+    'ac.',
+    'ac repair',
+    'ac Repair',
+    'AC REPAIR',
+    'AC repair',
+];
